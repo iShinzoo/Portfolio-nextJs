bb4128911ad81033a818337ce90042e1cfa14499
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2025-01-26 22:57:35 +0530

    system files

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <title>KRISHNA THAKUR</title>
-      <link rel="icon" href="/images/projects/logo.png" />
-      <Head />
+      <Head>
+        <title>KRISHNA THAKUR</title>
+        <link rel="icon" href="/images/projects/logo.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
